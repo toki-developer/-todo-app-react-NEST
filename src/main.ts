9ebe3e6095cca9bc906a71989e2f1c7d81fda310
Toki <55911995+tokio-k@users.noreply.github.com>
2021-03-21 09:58:55 +0900

    fix/cors対応

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
